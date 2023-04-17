05f634262da42fb4cef6f0d4ec3ccce7203fe551
firebro0 <firebroo@vip.163.com>
2023-04-17 21:30:31 +0800

    Update vahd.js

--- vahd.js
+++ vahd.js
@@ -15,21 +15,16 @@ console.log(`Address: ${hdNode.address} | Private Key: ${hdNode.privateKey} | Me
 
 let basePath = "m/44'/60'/0'/0";
 let wallets = [];
-const regex = /^0x1.*3$/ // 表达式
+const regex = /^0x1.*3$/
 console.log('Regex:', regex);
 
-for (let i = 0; i < 99999999999; i++) {
+var isValid = false
+while(!isValid){
     let hdNodeNew = hdNode.derivePath(basePath + "/" + i);
     let walletNew = new ethers.Wallet(hdNodeNew.privateKey);
 
-    if (regex.test(walletNew.address)) {
-    //    console.log(`Address： ${walletNew.address}`);
-    //    console.log(`Private key： ${walletNew.privateKey}`);
-        console.log(`Address: ${walletNew.address} | Private key: ${walletNew.privateKey}`);
-        break;
-    }
-    wallets.push(walletNew); // 不断生成
+    isValid = regex.test(walletNew.address)
+    //console.log(walletNew.address)
 }
 
-console.log("99999999999? One more time ...")
-//console.log(wallets);
+console.log(`Address: ${walletNew.address} | Private key: ${walletNew.privateKey}`);
