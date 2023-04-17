7d41072bf913814ccc5f5ce113c607a2beecc6ca
firebro0 <firebroo@vip.163.com>
2023-04-17 21:28:27 +0800

    Update va.js

--- va.js
+++ va.js
@@ -1,21 +1,14 @@
-// 正则表达式，
-// ^0x之后跟前几位要匹配的字符
-// .*为通配符
-// $之前写最后几位要匹配的字符
-// 例子：首位两个0，末尾两个1 
 // const regex = /^0x00.*11$/
 
 import { ethers } from "ethers";
 
-var wallet // 钱包
-const regex = /^0x1980.*0313$/ // 表达式 ^0x.*19800313.*$
+var wallet
+const regex = /^0x0000.*0000$/
 var isValid = false
 while(!isValid){
-    wallet = ethers.Wallet.createRandom() // 随机生成钱包，安全
-    isValid = regex.test(wallet.address) // 检验正则表达式
+    wallet = ethers.Wallet.createRandom()
+    isValid = regex.test(wallet.address)
     console.log(wallet.address)
 }
-// 打印靓号地址与私钥
-//console.log(`\nAddress：${wallet.address}`)
-//console.log(`Private Key：${wallet.privateKey}\n`)
+
 console.log(`Address: ${wallet.address} | Private Key: ${wallet.privateKey}`);
